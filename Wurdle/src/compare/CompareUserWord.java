327f12d3deaf71db03749c4072d8f7b1e7e689ad
Sydney <sydney.allison@wustl.edu>
2022-04-04 18:07:04 -0500

    working on comparing with for loop

--- Wurdle/src/compare/CompareUserWord.java
+++ Wurdle/src/compare/CompareUserWord.java
@@ -3,6 +3,18 @@ package compare;
 public class CompareUserWord {
 
     public boolean compare_words(String user_input, String generated_word) {
+        for(int charInString=0; charInString<user_input.length(); charInString++){
+            if(user_input.charAt(charInString) == generated_word.charAt(charInString)){
+                System.out.println(user_input.charAt(charInString));
+            }
+            for(int index=0; index<generated_word.length(); index++){
+                
+            if(user_input.charAt(charInString) == generated_word.charAt(index)){
+                System.out.println(charInString);
+
+            }
+            }
+        }
         boolean check; 
         if(user_input.equals(generated_word)){
             check = true;
