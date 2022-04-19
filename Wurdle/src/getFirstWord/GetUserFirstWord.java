18025d257bdd267cfd71a53436fb16ddf5e77d81
jordyngoldstein <jordyn.goldstein@wustl.edu>
2022-04-19 13:38:41 -0500

    Tolowercaseworks

--- Wurdle/src/getFirstWord/GetUserFirstWord.java
+++ Wurdle/src/getFirstWord/GetUserFirstWord.java
@@ -8,7 +8,8 @@ import java.util.Scanner;
     public static String userInput(int x, Scanner scanner, ArrayList<String> guesses){
         System.out.println("Please enter your guess");
         String userInputWord;
-        userInputWord = scanner.nextLine(); 
+        userInputWord = scanner.nextLine().toLowerCase();;
+        System.out.println(userInputWord); 
         while (userInputWord.length() != 5){
             System.out.println("try again, must be 5 letters");
             userInputWord = scanner.nextLine(); 
